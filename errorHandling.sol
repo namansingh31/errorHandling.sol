6c5b762fee82555177ab1e9b39c415b34b5b972f
Naman kumar <126199142+namansingh31@users.noreply.github.com>
2024-06-30 15:24:12 +0530

    Update errorHandling.sol

--- errorHandling.sol
+++ errorHandling.sol
@@ -1,43 +1,52 @@
+
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.5.0 < 0.9.0;
 
-contract TokenAgreement {
-    address private holder;
-    uint public overallSupply;
-    mapping (address => uint) public balances;
 
-    constructor() {
-        holder = msg.sender;
-        overallSupply = 100000;
-        balances[holder] = overallSupply;
+contract my_Unique_Bank {
+    address payable public holder;
+    uint256 public balances;
+
+    event Deposit(uint amount);
+    event Withdraw(uint amount);
+
+    constructor(uint initialBalance) payable {
+        holder = payable(msg.sender);
+        balances = initialBalance;
     }
 
-    function transfer(address receiver, uint amount) public {
-        require(msg.sender!= address(0), "The sender is invalid");
-        require(receiver!= address(0), "The receiver is invalid");
-        require(balances[msg.sender] >= amount, "the balance is not sufficient");
-        balances[msg.sender] -= amount;
-        balances[receiver] += amount;
+    function getBalance() public view returns(uint){
+        return balances;
     }
 
-    function mint(uint amount) public {
-        require(msg.sender == holder, "Tokens can only be mint by the holder.");
-        require(amount > 0, "Amount must be greater than 0");
-        overallSupply += amount;
-        balances[holder] += amount;
+    function deposit(uint _amount) public payable {
+        uint _previousBalance = balances;
+
+        require(msg.sender == holder, "You are not the holder of this account");
+
+        balances += _amount;
+
+        assert(balances == _previousBalance + _amount);
+
+
+        emit Deposit(_amount);
     }
 
-    function burn(uint amount) public {
-        require(msg.sender == holder, "Tokens can only burn by the holder");
-        require(amount > 0, "Amount must be greater than zero");
-        require(balances[holder] >= amount, "the balance is not sufficient");
-        overallSupply -= amount;
-        balances[holder] -= amount;
-        assert(balances[holder] >= 0);
-    }
+    error InsufficientBalance(uint balance, uint withdrawAmount);
 
-    function getBalance() public view returns (uint) {
-        assert(balances[msg.sender] >= 0);
-        return balances[msg.sender];
+    function withdraw(uint _withdrawAmount) public {
+        require(msg.sender == holder, "You are not the holder of this account");
+        uint _previousBalance = balances;
+        if (balances < _withdrawAmount) {
+            revert InsufficientBalance({
+                balance: balances,
+                withdrawAmount: _withdrawAmount
+            });
+        }
+
+        balances -= _withdrawAmount;
+        assert(balances == (_previousBalance - _withdrawAmount));
+        emit Withdraw(_withdrawAmount);
     }
 }
+
